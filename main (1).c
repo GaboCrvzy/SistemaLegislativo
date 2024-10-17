a1c5d137ca0bca6de5447c92e9476be070a29408
GaboCrvzy <gabriel.fuentes.s@mail.pucv.cl>
2024-10-17 19:19:34 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+struct Politico {
+    char *nombrePolitico; 
+    char *rut;
+    char *partido; 
+    struct ProyectoLey **proyectos;        // Proyectos en los que participa el político
+    int cantProyectos;
+};
+
+struct Ciudadano {
+    char *nombreCiudadano;              
+    char *rutCiudadano;                     
+    struct ProyectoLey **proyectosIniciados;  
+    int cantidadProyectos;
+};
+
+struct NodoParlamentario {
+    struct Politico *parlamentario;  
+    struct NodoParlamentario *ant, *sig; 
+};
+
+struct NodoCiudadano {
+    struct Ciudadano *ciudadano;
+    struct NodoCiudadano *sig;
+};
+
+struct TribunalConstitucional {
+    int esRevisado;          // 0 NO REVISADO, 1 REVISADO
+    int esConstitucional;    // 0 INCONSTITUCIONAL, 1 CONSTITUCIONAL
+};
+
+struct Voto {
+    struct Politico *parlamentario; 
+    int tipoVoto;                   // 1 = a favor, 2 = en contra, 3 = abstención
+};
+
+struct Votacion {
+    char *fechaVotacion;
+    int votosAFavor;     
+    int votosEnContra;    
+    int abstenciones; 
+    struct Voto **votos;
+    int totalVotos;
+    int resultado;            // 0 = Rechazado, 1 = Aprobado
+};
+
+struct ComisionMixta {
+    char *estadoActual;    
+    struct NodoParlamentario *miembros; 
+    struct Votacion votacion; 
+};
+
+struct Camara {
+    char *nombreDeCamara;                    
+    struct Comision *comisiones;     
+    struct Votacion votacionGeneral;    
+    struct Votacion votacionEspecifica; 
+};
+
+struct ProyectoLey {
+    int id;                         // Identificador UNICO del proyecto
+    char *tituloProyecto;  
+    char *iniciativaLegislativa; 
+    struct Camara camaraOrigen; 
+    struct Camara camaraRevisora;  
+    struct ComisionMixta *comisionMixta; 
+    struct TribunalConstitucional *controlConstitucional; 
+};
+
+struct NodoABB {
+    struct ProyectoLey *proyecto; 
+    struct NodoABB *izq;           // Proyectos con ID menor
+    struct NodoABB *der;           // Proyectos con ID mayor
+};
+
+struct SistemaLegislativo {
+    struct NodoABB *abbProyectos;          // ABB de proyectos de ley por ID
+    struct NodoParlamentario *diputados; 
+    struct NodoParlamentario *senadores; 
+    struct NodoCiudadano *ciudadanos;
+};
+
+struct Veto {
+    int tipoVeto;                // 1 si veto parcial o 2 si veto total
+    char *motivo;                     
+};
+
+struct Presidente {
+    char *nombrePresidente;              
+    struct Veto *veto;           // NULL si no ha vetado
+    struct SistemaLegislativo *sistema;
+};
